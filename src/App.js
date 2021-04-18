842a68dd591d5ed4ad1998a5ad5b38f685395c80
irinatu17 <ira_tushina@mail.ru>
2021-04-18 18:02:04 +0100

    React-icons installed, x icon added

--- src/App.js
+++ src/App.js
@@ -4,31 +4,35 @@ import Tasks from './components/Tasks'
 
 const App = () => {
   const [tasks, setTasks] = useState([
-    {
+      {
         id: 1,
         text: "Doctors Appointment",
         day: "Feb 5th at 2:30pm",
         reminder: true,
-    },
-    {
+      },
+      {
         id: 2,
         text: "Meeting at School",
         day: "Feb 6th at 1:30pm",
         reminder: true,
-    },
-    {
+      },
+      {
         id:3,
         text: "Food Shopping",
         day: "Feb 5th at 2:30pm",
         reminder: false,
-    }
-])
+      }
+  	])
 
+  // Delete Task
+  const deleteTask = (id) => {
+    console.log("delete", id)
+  }
 
   return (
     <div className="container">
      <Header />
-     <Tasks tasks={tasks}/>
+     <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
